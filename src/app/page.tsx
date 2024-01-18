f80c2a815ceb9239157a9402359640d8b9ba748d
Oluwatobi <amure387@gmail.com>
2024-01-18 09:09:00 +0100

    feat: added neccesary files, todo: fix attribution positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,96 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import data from "../../data.json"
+import { useState } from 'react'
+import './index.css'
+import { Rubik } from 'next/font/google'
 
-export default function Home() {
+const rubik = Rubik({subsets: ["latin"]})
+interface TimeFrame {
+  daily: Time,
+  weekly: Time,
+  monthly: Time,
+}
+interface Time {
+  current: number,
+  previous: number
+}
+
+function WorkBoard({ item, period, name }: {
+  item: { title: string, timeframes: TimeFrame },
+  period: "daily" | "weekly" | "monthly", name: string
+}) {
+  console.log(name);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <div className={`grid-item ${name}`}>
+      <div className="space">
+        <Image
+        className='activity-icon'
+          src={`/images/icon-${item.title.toLowerCase().split(" ").join("-")}.svg`}
+          width={50}
+          height={50}
+          alt='Activity icon' />
+      </div>
+      <div className={`item`}>
+        <div className="work">
+          <h4>{item.title}</h4>
+          <Image
+          src={`/images/icon-ellipsis.svg`}
+          width={30}
+          height={6}
+          alt='ellipsis icon'
+          className='ellipsis-icon' />
+        </div>
+        <div className="time">
+          <h3>{period === "daily"
+            ? item.timeframes.daily.current
+            : period === "weekly"
+              ? item.timeframes.weekly.current
+              : item.timeframes.monthly.current}hrs</h3>
+          <p>Last{period === "daily"
+            ? ` day - ${item.timeframes.daily.previous}`
+            : period === "weekly"
+              ? ` week - ${item.timeframes.weekly.previous}`
+              : ` month - ${item.timeframes.monthly.previous}`}hrs</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    </div>
+  )
+}
+export default function Home() {
+  const [period, setPeriod] = useState<"daily" | "weekly" | "monthly">("daily");
+  return (
+    <>
+    <main>
+      <div className="user grid-item">
+        <div className="user_details">
+          <Image src={'/images/image-jeremy.png'} alt={'a picture of jeremy'} width={60} height={60} />
+          <div className="user_intro">
+            <p>Report for</p>
+            <h2><span className="line1">Jeremy</span> Robson</h2>
+          </div>
+        </div>
+        <div className='period'>
+          <h3 onClick={() => setPeriod("daily")}>Daily</h3>
+          <h3 onClick={() => setPeriod("weekly")}>Weekly</h3>
+          <h3 onClick={() => setPeriod("monthly")}>Monthly</h3>
+        </div>
       </div>
+      {/* <div className="grid-container"> */}
+        {data.map((item, index) => <WorkBoard item={item}
+          key={item.title}
+          period={period}
+          name={`grid${index}`} />)}
+      {/* </div> */}
+      </main>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+      </>
   )
 }
